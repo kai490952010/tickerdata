468eefdee8dd1fbaf44d3479d9fc3e73ef2212a4
kai490952010 <kai490952010@gmail.com>
2021-05-05 08:57:58 +0530

    Moves meta functionality to separate module

--- tickerdata/meta.py
+++ tickerdata/meta.py
@@ -19,3 +19,29 @@ def get_sql_engine():
 	    }
 	)
 	return sql_engine
+
+
+class metaApi():
+    def __init__(self):
+        self.exchange = exchange
+
+    def get_min_max_ts(self, symbol, granularity, **kwargs):
+        """
+            Get min-max timestamps for available data of a coinpair,
+            to restart data collection from latest end timestamp.
+
+            Args:
+                symbol (str): symbol to get latest available timestamps for.
+                granularity (str): used to get SQL table because
+                    granularity will be a component of table name.
+                    Options: 1m, 5m, 30m, 1h, 4h, 1D, 1W, 1M
+
+            Returns:
+                tuple, (start_ts, end_ts)
+
+            TODO: 
+            	Implement functionality
+        """
+        self.symbol = kwargs.get('symbol')
+        self.granularity = kwargs.get('granularity')
+        raise NotImplementedError
